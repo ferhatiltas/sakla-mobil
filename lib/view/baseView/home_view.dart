71c774810bfdcb0867c14bf9157c98c6cde14d12
Cihat Karaboğa <hestu12@gmail.com>
2021-04-18 20:51:44 +0300

    add:image compress, image picker added

--- lib/view/baseView/home_view.dart
+++ lib/view/baseView/home_view.dart
@@ -1,5 +1,11 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:io';
+import 'dart:math';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+
+import 'package:path_provider/path_provider.dart' as path_provider;
+import 'package:flutter_image_compress/flutter_image_compress.dart';
 import 'package:sakla/core/components/app_bar_container.dart';
 
 class HomeView extends StatefulWidget {
@@ -8,6 +14,8 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  ImageProvider? provider;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,9 +23,22 @@ class _HomeViewState extends State<HomeView> {
       body: Column(
         children: [buildAppBar(), Expanded(flex: 4, child: Text(''))],
       ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () {
+      //     _testCompressFile();
+      //   },
+      // ),
     );
   }
 
+  // AspectRatio(
+  // child: Image(
+  // image: provider ?? AssetImage('assets/image/splash.png'),
+  // width: double.infinity,
+  // fit: BoxFit.contain,
+  // ),
+  // aspectRatio: 1 / 1,
+  // ),
   Expanded buildAppBar() {
     return Expanded(
       flex: 1,
@@ -31,5 +52,64 @@ class _HomeViewState extends State<HomeView> {
         ),
       ),
     );
+  }
+
+  Future<void> compress() async {
+    final img = AssetImage('image/splash.png');
+    print('pre compress');
+    final config = ImageConfiguration();
+
+    var key = await img.obtainKey(config);
+    final data = await key.bundle.load(key.name);
+
+    final beforeCompress = data.lengthInBytes;
+    print('beforeCompress = $beforeCompress');
+
+    final result =
+        await FlutterImageCompress.compressWithList(data.buffer.asUint8List());
+
+    print('after = ${result.length}');
+  }
+
+  void _testCompressFile() async {
+    final img = AssetImage('assets/image/splash.png');
+    print('pre compress');
+    final config = ImageConfiguration();
+
+    var key = await img.obtainKey(config);
+    final data = await key.bundle.load(key.name);
+    final dir = await path_provider.getTemporaryDirectory();
+    print('dir = $dir');
+
+    var file = createFile('${dir.absolute.path}/test.png');
+    file.writeAsBytesSync(data.buffer.asUint8List());
+
+    final result = await testCompressFile(file);
+
+    if (result == null) return;
+
+    ImageProvider provider = MemoryImage(result);
+    this.provider = provider;
+    setState(() {});
+  }
+
+  Future<Uint8List?> testCompressFile(File file) async {
+    final result = await FlutterImageCompress.compressWithFile(
+      file.absolute.path,
+      quality: 5,
+    );
+
+    print(file.lengthSync());
+    print((log(file.lengthSync()) / log(1024)).floor());
+    print(result?.length);
+    return result;
+  }
+
+  File createFile(String path) {
+    final file = File(path);
+    if (!file.existsSync()) {
+      file.createSync(recursive: true);
+    }
+    return file;
   }
 }
--- lib/view/baseView/photo_view.dart
+++ lib/view/baseView/photo_view.dart
@@ -1,6 +1,13 @@
+import 'dart:io';
+import 'dart:math';
+import 'dart:typed_data';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_image_compress/flutter_image_compress.dart';
 import 'package:sakla/core/components/app_bar_container.dart';
+import 'package:sakla/core/extension/context_extension.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
 
 class PhotoView extends StatefulWidget {
   @override
@@ -8,12 +15,13 @@ class PhotoView extends StatefulWidget {
 }
 
 class _PhotoViewState extends State<PhotoView> {
+  ImageProvider? provider;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[200],
       body: Column(
-        children: [buildAppBar(), Expanded(flex: 4, child: Text(''))],
+        children: [buildAppBar(), Expanded(flex: 4, child: Container())],
       ),
     );
   }
@@ -31,5 +39,64 @@ class _PhotoViewState extends State<PhotoView> {
         ),
       ),
     );
+  }
+
+  Future<void> compress() async {
+    final img = AssetImage('image/splash.png');
+    print('pre compress');
+    final config = ImageConfiguration();
+
+    var key = await img.obtainKey(config);
+    final data = await key.bundle.load(key.name);
+
+    final beforeCompress = data.lengthInBytes;
+    print('beforeCompress = $beforeCompress');
+
+    final result =
+        await FlutterImageCompress.compressWithList(data.buffer.asUint8List());
+
+    print('after = ${result.length}');
+  }
+
+  void _testCompressFile() async {
+    final img = AssetImage('assets/image/splash.png');
+    print('pre compress');
+    final config = ImageConfiguration();
+
+    var key = await img.obtainKey(config);
+    final data = await key.bundle.load(key.name);
+    final dir = await path_provider.getTemporaryDirectory();
+    print('dir = $dir');
+
+    var file = createFile('${dir.absolute.path}/test.png');
+    file.writeAsBytesSync(data.buffer.asUint8List());
+
+    final result = await testCompressFile(file);
+
+    if (result == null) return;
+
+    ImageProvider provider = MemoryImage(result);
+    this.provider = provider;
+    setState(() {});
+  }
+
+  Future<Uint8List?> testCompressFile(File file) async {
+    final result = await FlutterImageCompress.compressWithFile(
+      file.absolute.path,
+      quality: 5,
+    );
+
+    print(file.lengthSync());
+    print((log(file.lengthSync()) / log(1024)).floor());
+    print(result?.length);
+    return result;
+  }
+
+  File createFile(String path) {
+    final file = File(path);
+    if (!file.existsSync()) {
+      file.createSync(recursive: true);
+    }
+    return file;
   }
 }
